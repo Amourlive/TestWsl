3182f71f0639ed9ef457d8737ed05f2d0d2bed9a
Amourlive <aleksei_mackevich@outlook.com>
2018-03-07 21:36:58 +0300

    Initial commit

---
+++ order_parse.rb
@@ -0,0 +1,15 @@
+orders_line = 'product_1=2,product_2=2,product_3=3,'
+
+def parse_order_line(orders_line)
+  s1 = orders_line.split(/,/)
+  arr = []
+  s1.each do |x|
+    s2 = x.split(/=/)
+    s3 = s2[0].split(/_/)
+    id = s3[1]
+    cnt = s2[1]
+    arr2 = [id, cnt]
+    arr.push arr2
+  end
+end
+puts parse_order_line(orders_line).inspect
