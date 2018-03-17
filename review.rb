9e8142007b907c6811d5cec8624dd472c6807d35
Amourlive <aleksei_mackevich@outlook.com>
2018-03-17 22:16:19 +0300

    add method # find_adjective

--- review.rb
+++ review.rb
@@ -1,3 +1,13 @@
+def find_adjective(string)
+  words = string.split(' ')
+  index = words.find_index('is')
+  if index.nil?
+    nil
+  else
+    words[index + 1]
+  end
+end
+
 lines = []
 
 File.open('reviews.txt') do |review_file|
@@ -6,6 +16,10 @@ end
 
 relevant_lines = lines.find_all { |line| line.include?('Truncated') }
 reviews = relevant_lines.reject { |line| line.include?('--') }
-
 puts reviews
-
+string = reviews.first
+words = string.split(' ')
+index = words.find_index('is')
+puts index
+adjectives = reviews.map { |review| find_adjective(review) }
+p adjectives
