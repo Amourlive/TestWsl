6cb8a1d781a0d60d4fde6546bdbfc32fa683b944
Amourlive <aleksei_mackevich@outlook.com>
2018-03-17 21:13:50 +0300

    unnecessary comments

--- review.rb
+++ review.rb
@@ -4,4 +4,8 @@ File.open('reviews.txt') do |review_file|
   lines = review_file.readlines
 end
 
-puts lines.length
+relevant_lines = lines.find_all { |line| line.include?('Truncated') }
+reviews = relevant_lines.reject { |line| line.include?('--') }
+
+puts reviews
+
