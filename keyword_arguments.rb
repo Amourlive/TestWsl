1fff57df63b91498d47db3478fa4c58821714662
Amourlive <aleksei_mackevich@outlook.com>
2018-03-18 18:10:52 +0300

    add class Candidate and method #initialize with keyword arguments

---
+++ keyword_arguments.rb
@@ -0,0 +1,10 @@
+class Candidate
+  attr_accessor :name, :age, :occupation, :hobby, :birthplace
+  def initialize(name, age:, occupation:, hobby: nil, birthplace: 'Sleepy Creek')
+    self.name = name
+    self.age = age
+    self.occupation = occupation
+    self.hobby = hobby
+    self.birthplace = birthplace
+  end
+end
