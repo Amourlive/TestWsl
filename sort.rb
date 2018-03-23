399df6cb0310dc50d41ecad6d0bdc3ea161e09c2
Amourlive <aleksei_mackevich@outlook.com>
2018-03-23 15:02:42 +0300

    add #bubble_sorting

--- sort.rb
+++ sort.rb
@@ -1,19 +1,34 @@
-arr = [1, 0, 3, 2, 4, 9, 7, 6, 5, 8]
+arr = [8, 0, 3, 2, 4, 9, 7, 6, 5, 1]
 
 def stupid_sort(arr)
-  number = 0
+  iteration = 0
   i = 0
   loop do
-    number += 1
     if arr[i] > arr[i + 1]
       arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      iteration += 1
       i = 0
     end
     i += 1
     break if i == (arr.length - 2)
   end
-  p number
+  p iteration
+  arr
+end
+p stupid_sort(arr) # 14 iteration on 10 elements
+
+def bubble_sorting(arr)
+  iteration = 0
+  (0..arr.length - 1).each do |i|
+    (i + 1..arr.length - 1).each do |j|
+      if arr[i] > arr[j]
+        arr[i], arr[j] = arr[j], arr[i]
+        iteration += 1
+      end
+    end
+  end
+  p iteration
   arr
 end
 
-p stupid_sort(arr)
+p bubble_sorting(arr) # 8  iteration on 10 elements
