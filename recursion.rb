c154227bddea3e0113ff0919c36930d3d6ebe866
Amourlive <aleksei_mackevich@outlook.com>
2018-03-23 09:26:29 +0300

    add bottles method

--- recursion.rb
+++ recursion.rb
@@ -39,4 +39,13 @@ def palindrome(string)
   end
 end
 
-p palindrome('opopo')
+def bottles(n)
+  if n.zero?
+    puts 'no more bottles of beer on the wall'
+  else
+    puts "#{n} bottles of beer on the wall"
+    bottles(n - 1)
+  end
+end
+
+bottles(6)
